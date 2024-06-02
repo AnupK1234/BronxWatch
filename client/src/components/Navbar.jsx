566179ee1f055f74a90c1d986843ccbda70df32e
Abhinav Sharma <118145401+abssharma@users.noreply.github.com>
2024-06-01 22:53:31 -0700

    Update Navbar.jsx

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
             <NavLink to = "/" className = "w-10 h-10 rounded-lg bg-white items-center justify-center flex font-bold shadow-md">
                 <p className = "blue-gradient_text">BRONX WATCH</p>
             </NavLink>
-            <nav className = "flex text-lg gap-7 font-medium">
+            <nav className = "flex text-lg gap-5 font-medium">
                 <NavLink to = "/register_complaints" className = {({ isActive }) => isActive ? 
                 'text-blue-500' : 'text-black'}>
                     Register Complaint
@@ -20,6 +20,18 @@ const Navbar = () => {
                 <NavLink to = "/about" className = {({ isActive }) => isActive ? 
                 'text-blue-500' : 'text-black'}>
                     About
+                </NavLink>
+                <NavLink to = "/signup" className = {({ isActive }) => isActive ? 
+                'text-blue-500' : 'text-black'}>
+                    Sign Up
+                </NavLink>
+                <NavLink to = "/signin" className = {({ isActive }) => isActive ? 
+                'text-blue-500' : 'text-black'}>
+                    Log In
+                </NavLink>
+                <NavLink to = "/dashboard" className = {({ isActive }) => isActive ? 
+                'text-blue-500' : 'text-black'}>
+                    Dashboard
                 </NavLink>
                 <NavLink to = "/contact" className = {({ isActive }) => isActive ? 
                 'text-blue-500' : 'text-black'}>
